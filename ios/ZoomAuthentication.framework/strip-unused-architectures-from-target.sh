0bdfa7c8a1f27be474127b295178199bddb09a62
Willian Ribeiro Angelo <agfoccus@gmail.com>
2018-01-30 16:35:39 -0200

    fix(path/ios): Fix path for iOS Framework

--- ios/ZoomAuthentication.framework/strip-unused-architectures-from-target.sh
+++
@@ -1,1 +0,0 @@
-../../.git/annex/objects/f1/VW/SHA256E-s2202--91bab67693420b1a0722a54064ab2bb0259435de45bc702fdfce897b5ebfd6ad.sh/SHA256E-s2202--91bab67693420b1a0722a54064ab2bb0259435de45bc702fdfce897b5ebfd6ad.sh
